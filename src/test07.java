429512b1f5cadb3b76345d52566a1175365cf292
JinYong <jinyong126abc@126.com>
2019-11-19 23:55:10 +0800

    333

--- src/test07.java
+++ src/test07.java
@@ -3,4 +3,7 @@ public class test07 {
     public static void main(String[] args) {
         System.out.println("this is 07");
     }
+    public void  test (){
+        System.out.println("this is test method");
+    }
 }
